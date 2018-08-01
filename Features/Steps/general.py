2904de2585357027850957ecaa076357bcf7e94d
Oksana P <oxana-popova@mail.ru>
2018-08-01 15:44:43 +0300

    add a link check

--- Features/Steps/general.py
+++ Features/Steps/general.py
@@ -1,36 +1,62 @@
-import self as self
-from behave import when, given, then, step
-from behave.runner import Context
-from selenium.common.exceptions import NoSuchElementException, JavascriptException, ElementNotVisibleException
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+import csv
+from selenium.webdriver.support import expected_conditions as ec
+from behave import when, given, then
+from selenium.common.exceptions import ElementNotVisibleException
+
+CRED = "wasite:wasite"
+
+
+def normalize_url(url: str) -> str:
+    return url.replace(f"{CRED}@", "")
+
 
 @given(u'we on page "{page}"')
-def step_impl(context, page):
-    #page = "http://www.wave-access.com/"
+def step_page(context, page):
+    # page = "http://www.wave-access.com/"
     # context.driver.get(page)
-        context.driver.get('http://wasite:wasite@wasiteen.wavea.cc/')
+    context.driver.get(f'http://{CRED}@wasiteen.wavea.cc/')
+    context.driver.maximize_window()
+
+
+@when(u'we click at sections')
+def step_btn(context):
+
+    def csv_dict_reader(file_obj):
+        reader = csv.DictReader(file_obj, delimiter=',')
+        for line in reader:
+            context.driver.find_element_by_xpath(f'//a[contains(text(),"{(line["section"])}")]').click()
+            url = context.driver.current_url
+            need_url = f'http://{CRED}@wasiteen.wavea.cc/public_en/{(line["sections"])}.aspx'
+            assert url == need_url
+
+    with open("resources/menu.csv") as f_obj:
+        csv_dict_reader(f_obj)
 
 
 @when(u'we click at button "{button}"')
-def step_impl(context, button):
+def step_btn(context, button):
     context.driver.find_element_by_xpath(f'//a[contains(text(),"{button}")]').click()
-    #context.driver.find_element_by_css_selector('a[href*=\'button\'][class=\'skrollable skrollable-between\']').click()
-#http://www.wave-access.com/public_en/contacts.aspx
 
 
-@then(u'I should see the transition to "{sections}"')
-def step_impl(context, sections):
+@then('in section "{products}" we click at button details and we should see the transition to "{sections}"')
+def step_det(context, products, sections):
+    wait = WebDriverWait(context.driver, 5)
+    context.current_window = context.driver.current_window_handle
+    context.old_windows = context.driver.window_handles
+    context.driver.find_element_by_xpath(f'//h3[@class="h3-wa textUnder" and contains(text(), "{products}")]/parent::node()//a').click()
+    wait.until(ec.new_window_is_opened(context.old_windows))
+    new_window = [i for i in context.driver.window_handles if i not in context.old_windows]
+    context.driver.switch_to.window(new_window[0])
     url = context.driver.current_url
-    if url == context:
-        True
+    context.driver.close()
+    context.driver.switch_to.window(context.current_window)
+    assert normalize_url(url) == sections
 
 
 @when(u'we click at button phone call with id "{button}"')
-def step_impl(context, button):
-     context.driver.find_element_by_xpath(f'//*[@id="orderCallLink"]').click()
+def step_phn(context, button):
+    context.driver.find_element_by_xpath(f'//*[@id="orderCallLink"]').click()
 
 
 @when('we write "{text}" at element with id "{id}"')
@@ -39,20 +65,23 @@ def verify_title(context, text, id):
 
 
 @when(u'we write "" at element with id "{id}"')
-def step_impl(context, id):
+def step_verify(context, id):
     pass
 
 
 @when('we click at cell checkmark')
-def verify_title(context):
+def step_check(context):
     try:
-        context.driver.find_element_by_xpath('//div[@id="formGetStartedBox"]//input[@name="AgreePrivacyPolicy"]/parent::node()/span[@class="checkmark"]').click()
+        context.driver.find_element_by_xpath(
+            '//div[@id="formGetStartedBox"]//input[@name="AgreePrivacyPolicy"]'
+            '/parent::node()/span[@class="checkmark"]'
+        ).click()
     except ElementNotVisibleException:
         context.driver.find_element_by_class_name("checkmark").click()
 
 
 @when('we click at button submit')
-def verify_title(context):
+def step_click(context):
     try:
         context.driver.find_element_by_id('sendCallOrder').click()
     except ElementNotVisibleException:
@@ -60,35 +89,15 @@ def verify_title(context):
 
 
 @then('I should see field with id "{id}" and placeholder message "{error}"')
-def step_impl(context, id, error):
-    # b = context.driver.find_element_by_xpath('//div[contains(@class, \'alert-success\')]')
-    # assert message in b.text
-    #context.driver.find_element_by_placeholder(message)
-    #context.driver.find_element_by_id('TelephoneOrderCall')
+def step_msg(context, id, error):
     context.driver.find_element_by_xpath(f'//input[@id="{id}"][@placeholder="{error}"]')
 
 
-@when('in section "{products}" we click at button details')
-def verify_title(context, products):
-    elements = context.driver.find_elements_by_css_selector('h3.h3-wa.textUnder')
-    for item in elements:
-        if item.text == products:
-            item.parent.find_element_by_xpath('//a[@class="gradientBtn"][//*[@href]]').click()
-            break
-
-
-    #context.driver.find_element_by_css_selector('div.visible-lg > a.gradientBtn > span.text').click()
-    #context.driver.find_element_by_xpath('//div[@class="leftPart"][//*[@href]]').click()
-    # context.driver.find_element_by_xpath('//a[@class="gradientBtn"][//*[@href]]').click()
-    #context.driver.find_element_by_xpath('//a[@class="gradientBtn"]').click()
-    #context.driver.find_element_by_xpath('//a[span[text()[contains(., \'DETAils\')]]] ')
-
-
 @when('we click at button Get started')
-def verify_title(context):
-        context.driver.find_element_by_id('getStartedLink').click()
+def step_start(context):
+    context.driver.find_element_by_id('getStartedLink').click()
 
 
 @then('I should see message "{success}" at element with id "{id}"')
-def step_impl(context, success, id):
+def step_scs(context, success, id):
     context.driver.find_element_by_xpath(f'//div[@id="{id}"]/div[contains(text(),"{success}")]')
